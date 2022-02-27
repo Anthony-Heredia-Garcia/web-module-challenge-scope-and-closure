7bad50a01ea8a0bceb33e5f156315cc581e2a3ec
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-02-27 18:33:49 -0500

    Learning concat arrays

--- index.js
+++ index.js
@@ -220,3 +220,15 @@ function positiveSum(arr) {
   }
   return sum;
 }
+
+function concatArray(what) {
+
+  let newArray = [].concat.apply([], what);
+  return newArray
+}
+const bigArray = [[1, 2, 3], [4, 5], [6], ["stuff"]];
+const biggerArray = [[7], [8], [9, 10, 11, 12]];
+
+
+console.log(concatArray(biggerArray));
+
